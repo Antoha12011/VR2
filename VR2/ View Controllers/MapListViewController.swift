32470637702e7e278b3241676c45dbd1dadfba34
Antoha12011 <toni.pavlov.85@bk.ru>
2022-04-16 13:51:25 +0300

    Initial Commit

---
+++ VR2/ View Controllers/MapListViewController.swift
@@ -0,0 +1,72 @@
+//
+//  MapListViewController.swift
+//  VR2
+//
+//  Created by Anton Pavlov on 14.04.2022.
+//
+
+import UIKit
+
+class MapListViewController: UITableViewController, UISearchBarDelegate {
+
+    
+let data = ["Гарантия на Карякина", "Лучший", "Москва", "Кубанский", "Тургенев", "Большой", "Ревьера", "Оскар" , "Сказка", "Оникс"]
+    
+    
+    
+    
+@IBOutlet weak var searchBar: UISearchBar!
+    
+    var filteredData: [String]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        searchBar.delegate = self
+        
+        filteredData = data
+        
+    }
+
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return filteredData.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cello = tableView.dequeueReusableCell(withIdentifier: "cello")! as UITableViewCell
+        
+        cello.textLabel?.text = filteredData[indexPath.row]
+        
+        return cello
+    }
+    
+    // Этот код отвечает за работу поисковика
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        
+    filteredData = []
+        
+        if searchText == "" {
+         filteredData = data
+        }
+        else {
+        for list in data {
+            
+            if list.lowercased().contains(searchText.lowercased()) {
+            filteredData.append(list)
+            }
+        }
+        }
+        self.tableView.reloadData()
+    }
+}
+
+
